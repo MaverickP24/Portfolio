8a9aa1661c86459ff6dc4cd264de6e3a78659508
) <pratyushparida36@gmail.com>
2025-02-20 02:04:55 +0530

    page --> landingPg

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import "./index.css"
-import Page1 from './pages/Page1'
-// import Navbar from './components/navbar'
+// import Page1 from './pages/Page1'
+// // import Navbar from './components/navbar'
 import Page2 from './pages/Page2'
 import Skills from './pages/Skills'
+import LandingPg from './pages/LandingPg'
 
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
     <>
       <div>
         {/* <Navbar/> */}
-        <Page1/>
+        <LandingPg/>
         <Page2 />
         <Skills/>
       </div>

009865f877fd8fe372a041ab3fd83ce7ef863bbe
) <pratyushparida36@gmail.com>
2025-02-20 10:04:50 +0530

    issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import "./index.css"
-// import Page1 from './pages/Page1'
-// // import Navbar from './components/navbar'
 import Page2 from './pages/Page2'
 import Skills from './pages/Skills'
 import LandingPg from './pages/LandingPg'
@@ -11,7 +9,6 @@ const App = () => {
   return (
     <>
       <div>
-        {/* <Navbar/> */}
         <LandingPg/>
         <Page2 />
         <Skills/>
